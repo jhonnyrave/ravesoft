e451821ebfd75107859ad299c210c72cb081445a
jhonnyrave <jhonnyrave09@hotmail.com>
2024-02-07 00:07:55 -0500

    segundo comit

--- database/migrations/2024_02_07_032104_create_actividades_table.php
+++ database/migrations/2024_02_07_032104_create_actividades_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('actividades', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->date('date_of_birth');
+            $table->date('fecha_actividad');
+            $table->decimal('valor', 10, 2);
             $table->boolean('status')->default(true);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('actividades');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users/username/{username}', [UserController::class, 'showByUsername']);
 
 /*
 |--------------------------------------------------------------------------
