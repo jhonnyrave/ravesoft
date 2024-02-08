91ddd5d8b2e8be8e3239a0e6db3b3973890ec752
jhonnyrave <jhonnyrave09@hotmail.com>
2024-02-07 21:00:28 -0500

    yyy

--- config/config.php
+++ config/config.php
@@ -20,6 +20,7 @@ define("PATH_RAIZ",$path_raiz);
 define("PATH_DIRECTORIO",PATH_RAIZ.DIRECTORIO);
 
 $file=CORE.$_SERVER['SERVER_NAME'].".ini";
+die($file);
 if(!isset($_SESSION['config'])){
 	if(file_exists($file)){
 		$_SESSION['config']=parse_ini_file($file,true);
