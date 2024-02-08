a3cc7a861e53e1513ec155f473b4d4b2c248fb51
jhonnyrave <jhonnyrave09@hotmail.com>
2024-02-07 20:47:30 -0500

    eee

--- rest.php
+++ rest.php
@@ -2,9 +2,7 @@
 session_start();
 
 include_once("config/config.php");
-die('holaaaa');
 include_once(APP_MODELS."mainModel.php");
-
 include_once(APP_BUSINESS."mainBusiness.php");
 include_once(APP_CONTROLLERS."mainController.php");
 include_once(APP_TEMPLATE."mainTemplate.php");
